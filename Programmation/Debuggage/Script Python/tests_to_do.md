4c626919311ce482d200316fc5cccefba6f16f52
Sammy NP <sapar82#@ulaval.ca>
2023-04-20 00:08:42 -0400

    Notebook *relativement* complété

---
+++ Programmation/Debuggage/Script Python/tests_to_do.md
@@ -0,0 +1,7 @@
+## Tests à effectuer:
+\begin{itemize}
+\item \textbf{Test 1:} Test échelon 5W centré à x,y = 0,0 pour 30 secondes
+\item \textbf{Test 2:} Test échelon 5W centré à x,y = 0,5 pour 30 secondes
+\item \textbf{Test 3:} Test échelon 5W centré à x,y = 0,-5 pour 30 secondes
+\item \textbf{Test 4:} Test échelon 5W centré à x,y = 5,0 pour 30 secondes
+\item \textbf{Test 5:} Test échelon 5W centré à x,y = -5,0 pour 30 secondes
--- Programmation/Debuggage/Script Python/utils.py
+++ Programmation/Debuggage/Script Python/utils.py
@@ -2,6 +2,7 @@ from scipy.optimize import curve_fit
 import matplotlib.pyplot as plt
 import numpy as np
 import os
+import pandas as pd
 
 dataPath = os.path.dirname(os.path.abspath(__file__)) + "/vrai_test_escalier_17_avril.txt"
 
@@ -93,6 +94,7 @@ def datas(dataPath):
 	mux4 = thermistances[48:64]
 	return (time, mux1, mux2, mux3, mux4)
 
+
 def VtoR(v):
 	return -1800*v/(v-5)
 
@@ -103,6 +105,67 @@ def VtoT(v):
 def degree3(x,a,b,c,d):
 	return a*(x**3) + b*(x**2) + c*x + d
 
+def derivative(x, delta = 5):
+    """ 
+    Return dx/dy
+    x : array of values
+    return : array of derivative values
+    """
+    x = np.array(x)
+    return (x[delta:] - x[:-delta])
+
+def predictive_algo(x, c, d, delta, separation= False):
+	""" 
+	Return x ,c * dy/dx , d * d2x/dy2
+	x : array of values
+	c : array of coefficients
+	d : delta of coefficients
+	return : array of predicted values
+	"""
+	x = np.array(x)
+
+	if separation:
+	    return x[5:-5], c * derivative(x, delta[0])[5:], d * derivative(derivative(x, delta[1]), delta[2])
+	else:
+		return x[5:-5]+ c * derivative(x, delta[0])[5:]+ d * derivative(derivative(x, delta[1]), delta[2])
+
+def load_data(datapath):
+	"""
+	Load the data from the text file and return a dataframe
+	"""
+	columns = ["time", "power", "x", "y", "tension", 'temp']
+
+	indiceMux1 = [43, 42, 41, 40, 24, 23, 22, 10, 39, 21, 9, 3, 20, 8, 2, 1]
+	indiceMux2 = [101, 102, 37, 38, 59, 36, 58, 57, 19, 56, 35, 18, 55, 34, 17, 7]
+	indiceMux3 = [16, 33, 54, 32, 53, 31, 52, 51, 50, 30, 15, 6, 49, 29, 14, 5]
+	indiceMux4 = ["ref1", "ref2", "ref3", 4, 13, 28, 48, 11, 12, 27, 47, 46, 45, 26, 44, 25]
+	#stack all the indices in one array
+	datacolumns = np.concatenate((columns, indiceMux1, indiceMux2, indiceMux3, indiceMux4), dtype=str)
+	data = pd.read_csv(datapath, sep="\t", skiprows=1)
+	data.columns = datacolumns
+	data = data.replace(',','.', regex=True)
+	data = data.astype(float)
+	return data
+
+def rank_temperature(data_temp):
+    """
+    Return the index of the sensor with the highest temperature
+    to the lowest temperature in a list mid way through the data
+    """
+    data_temp = data_temp.drop(['time', 'power', 'x', 'y', 'tension', 'temp'], axis=1).copy()
+    data_temp = data_temp.iloc[len(data_temp)//2]
+    data_temp = data_temp.sort_values(ascending=False)
+    return data_temp.index
+
+def convert_to_temp(data):
+    """
+    Create a new dataframe with the temperature in Celsius
+    """
+    data_temp = data.copy()
+    for i in range(1, 59):
+        if str(i) in data_temp.columns:
+            data_temp[str(i)] = VtoT(data[str(i)])
+    return data_temp
 
 if __name__ == "__main__":
 	# arrayAcquisition = [8,19,20,22,3,9,12,18,21,23,2,6,10,13,24,26,25,1,5,7,14,31,27,29,28,0,4,55,11,15,43,30,33,34,62,63,56,51,47,32,35,36,60,61,52,46,42,37,38,59,57,53,45,41,39,58,54,44,40]
@@ -116,7 +179,7 @@ if __name__ == "__main__":
 	indiceMux3 = [16, 33, 54, 32, 53, 31, 52, 51, 50, 30, 15, 6, 49, 29, 14, 5]
 	indiceMux4 = [103, 104, 105, 4, 13, 28, 48, 11, 12, 27, 47, 46, 45, 26, 44, 25]
 
-	time, mux1, mux2, mux3, mux4 = datas()
+	time, mux1, mux2, mux3, mux4 = datas(dataPath)
 	indiceAberant = indicesAberant(indiceMux1, indiceMux2, indiceMux3, indiceMux4, mux1, mux2, mux3, mux4)
 	indiceDansMux4 = indiceAberantDansMux4(indiceAberant, indiceMux4)
 
